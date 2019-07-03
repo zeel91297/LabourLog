64d29d45895790c3a17210d16b4ffed0fb47b816
Zeel Modi <zeel91297@gmail.com>
2019-07-03 20:50:58 +0530

    job roles crud operation

--- labourLogAPIs/app.js
+++ labourLogAPIs/app.js
@@ -9,6 +9,7 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var WorkForceRouter = require('./routes/workforces_routes');
+var JobRolesRouter = require('./routes/jobroles_routes');
 
 
 var app = express();
@@ -27,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/workforces', WorkForceRouter);
+app.use('/jobroles', JobRolesRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
