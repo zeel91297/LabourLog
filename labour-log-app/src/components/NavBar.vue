c7e522c852d1b8ff6ba07199990c5775b4bbea05
Zeel Modi <zeel91297@gmail.com>
2019-07-04 15:26:24 +0530

    toolbar added to vue app

---
+++ labour-log-app/src/components/NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-app id="inspire">
+      <v-toolbar app dark color="primary">
+        <v-toolbar-title @click="onClickWorkForces" class="white--text">Labour Log</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down white--text">
+          <v-btn flat @click="onClickSources">Sources</v-btn>
+          <v-btn @click="onClickWorkForces" flat>Work Forces</v-btn>
+          <v-btn @click="onClickClients" flat>Clients</v-btn>
+        </v-toolbar-items>
+        <v-menu class="hidden-sm-and-up">
+          <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+          <v-list>
+            <v-list-tile @click="onClickSources">
+              <v-list-tile-content>
+                <v-list-tile-title>Sources</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile @click="onClickWorkForces">
+              <v-list-tile-content>
+                <v-list-tile-title>Work Forces</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile @click="onClickClients">
+              <v-list-tile-content>
+                <v-list-tile-title>Clients</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
+      </v-toolbar>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import router from "../router";
+export default {
+  methods: {
+    onClickSources() {
+      router.push({ name: "sources" });
+    },
+    onClickWorkForces() {
+      router.push({ name: "workforces" });
+    },
+    onClickClients() {
+      router.push({ name: "clients" });
+    }
+  }
+};
+</script>
---
+++ labour-log-app/src/views/Workforces.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-spacer></v-spacer>
+    <h1>List of WorkForces</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
