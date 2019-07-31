a689fbd960aae899ae747f2fffd5927c729c8845
Mohit Shah <shahmohit9975@gmail.com>
2019-07-31 13:53:18 +0530

    workforce - in calender workforce-working-details updated API added with changes [update: work_hours and client_id]

--- labourLogAPIs/models/workforces_calender_working_details_update.js
+++ labourLogAPIs/models/workforces_calender_working_details_update.js
@@ -6,7 +6,7 @@ var WorkForcesWorkingDetailsUpdateByDate = {
         console.log(idClientAndHours);
 
         // return db.query("SELECT * from clients_workforces cw, workforces_working_details ww where cw.workforce_id=? and ww.work_date=? and ww.client_workforce_id=cw.client_workforce_id", [idAndDate.workforce_id,idAndDate.work_date], callback);   
-        return db.query("UPDATE workforces_working_details as wwd JOIN clients_workforces as cw on (wwd.client_workforce_id=cw.client_workforce_id) set work_hours=? ,work_date=? WHERE workforce_work_id=?", [idClientAndHours.work_hours, idClientAndHours.work_date, idClientAndHours.workforce_id], callback);
+        return db.query("UPDATE workforces_working_details as wwd JOIN clients_workforces as cw on (wwd.client_workforce_id=cw.client_workforce_id) set work_hours=? ,cw.client_id=? WHERE workforce_work_id=?", [idClientAndHours.work_hours, idClientAndHours.client_id, idClientAndHours.workforce_work_id], callback);
     }
 };
 
