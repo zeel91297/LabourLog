acd1f20fed78866da140e954c0329cbb54f06159
Mohit Shah <shahmohit9975@gmail.com>
2019-08-13 00:52:19 +0530

    all done

--- labour-log-app/src/views/Flip.vue
+++ labour-log-app/src/views/Flip.vue
@@ -201,7 +201,16 @@ export default {
     }
   },
   head: {
-bt ,
+    script: [
+      {
+        src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"
+      }
+    ]
+  },
+  data() {
+    return {
+      myid: this.$route.params.id,
+      active: [],
       avatar: null,
       open: [],
       users: [],
